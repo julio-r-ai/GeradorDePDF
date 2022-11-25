0a00c9a52b0251988a96507e20912ea12e2808e3
Julio Cesar R Reis <julioreis044@gmail.com>
2022-11-25 17:40:06 -0300

    Iniciando projeto

---
+++ asserts/script.js
@@ -0,0 +1,9 @@
+function converterImagem(){ //função convertaer imagem
+
+    var receberArquivo =  document.querySelector("#imagem").files;
+    console.log(receberArquivo);
+
+    if(receberArquivo.length > 0){
+            
+    }
+}
