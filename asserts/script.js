893d475d0b278643cf0183a7ff976389c728677e
Julio Cesar <julioreis044@gmail.com>
2022-11-25 22:10:58 -0300

    Criei uma if para ferificar se o arquivo tem uma imagem

--- asserts/script.js
+++ asserts/script.js
@@ -1,9 +1,17 @@
 function converterImagem(){ //função convertaer imagem
 
-    var receberArquivo =  document.querySelector("#imagem").files;
-    console.log(receberArquivo);
+    //imprime o tamanho do arquivo
+    let receberArquivo =  document.querySelector("#imagem").files;
+    console.log(receberArquivo); 
 
-    if(receberArquivo.length > 0){
-            
-    }
-}
+    //imprime os dados sobre o arquivos
+    if(receberArquivo.length > 0){ 
+        let carregarImagem = receberArquivo[0];
+        console.log(carregarImagem);  
+        
+        let lerArquivo = new FileReader();
+
+        
+    };
+
+};
