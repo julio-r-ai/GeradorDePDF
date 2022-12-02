7f2106ef2552e7a06504c9bc7ca46387273aa1c7
Julio Cesar <julioreis044@gmail.com>
2022-12-01 23:04:17 -0300

    Desenvolvendo o gerador com o jsPDF

--- asserts/js/script.js
+++ asserts/js/script.js
@@ -3,24 +3,28 @@ const {default: jsPDF} = require("jspdf");
 function converterImagem(){ //função convertaer imagem
 
     //imprime o tamanho do arquivo
-    let receberArquivo =  document.querySelector("#imagem").files;
-    console.log(receberArquivo); 
+    let receberArquivo =  document.querySelector("#imagem").files; 
 
     //imprime os dados sobre o arquivos
     if(receberArquivo.length > 0){ 
-        let carregarImagem = receberArquivo[0];
-        console.log(carregarImagem);  
+        let carregarImagem = receberArquivo[0]; 
         
        let arquivoLer = new FileReader();
 
         arquivoLer.onload = function(arquivoCarregadoEvento){
-            let srcDados = arquivoCarregadoEvento.target.result;
-            console.log(srcDados);  
-        }
-        let doc = new jsPDF();
+            let imagemBase64 = arquivoCarregadoEvento.target.result;
 
-        doc.text('arquivoEmPDF');
-        doc.addImage();
+            let doc = new jsPDF();
+
+            doc.text('pdfGerado', 15, 20);
+
+            doc.addImage(imagemBase64, 'JPEG', 15, 40, 300, 157);
+
+            doc.save('julio.pdf')
+
+        }
+        
+        arquivoLer.readAsDataURL(carregarImagem);
 
     };
 
